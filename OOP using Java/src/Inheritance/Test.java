8252f1eff1accd1705e493d6baf1bfacd86bc7be
Hasibull <hasibulislam4500@gmail.com>
2022-01-11 23:34:39 +0600

    Modifying Inheritance package

--- OOP using Java/src/Inheritance/Test.java
+++ OOP using Java/src/Inheritance/Test.java
@@ -3,9 +3,10 @@ package Inheritance;
 
 public class Test {
     public static void main(String[] args) {
-        Animal cat = new Cat();
+        Cat cat = new Cat();
         
         cat.eating();
         cat.sleeping();
+        cat.voice();
     }
 }
