34cbe53ff2c5e91780263a11056a1ee4142d2c4f
Hasibull <hasibulislam4500@gmail.com>
2022-01-21 23:22:00 +0600

    Polymorphism- method overloading

--- OOP using Java/src/Polymorphism/MethodOverloading.java
+++ OOP using Java/src/Polymorphism/MethodOverloading.java
@@ -2,18 +2,18 @@
 package Polymorphism;
 
 public class MethodOverloading {
-    static double sum(double a, double b){
+    public static double sum(double a, double b){
         return a+b;
     }
-    static int sum(int a, int b){
+    public static int sum(int a, int b){
         return a+b;
     }
-    static String sum(String a, String b){
+    public static String sum(String a, String b){
         return a+b;
     }
     public static void main(String[] args) {
         double s = sum(10.12, 13.22);
         
-        System.out.println("The float sum is = " + sum);
+        System.out.println("The float sum is = " + s);
     }
 }
