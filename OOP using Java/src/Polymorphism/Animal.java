9e56759749ccee9f6114c50a7efbb051a98145ae
Hasibull <hasibulislam4500@gmail.com>
2022-02-13 23:50:36 +0600

    Method overriding

---
+++ OOP using Java/src/Polymorphism/Animal.java
@@ -0,0 +1,11 @@
+
+package Polymorphism;
+
+public class Animal {
+    public void eat(){
+        System.out.println("Eating...............");
+    }
+    public void running(){
+        System.out.println("Running............");
+    }
+}
