34a2b13a43450ef7cf15c65d99ee3305cab001d2
Hasibull <hasibulislam4500@gmail.com>
2022-02-13 23:55:31 +0600

    Overriding method's from Animal class to Cat class

---
+++ OOP using Java/src/Polymorphism/Cat.java
@@ -0,0 +1,13 @@
+
+package Polymorphism;
+
+public class Cat extends Animal {
+    @Override
+    public void eat(){
+        System.out.println("Cat is eating.................");
+    }
+    @Override
+    public void running(){
+        System.out.println("Cat is running..............");
+    }
+}
