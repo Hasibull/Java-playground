3b2efe29bffd7cf7c0c671d25fd84e8f5b451889
Hasibull <hasibulislam4500@gmail.com>
2022-12-05 15:45:34 +0600

    Educational institute management system

---
+++ OOP using Java/src/Basic/Test.java
@@ -0,0 +1,14 @@
+
+package Basic;
+
+public class Test {
+    static int weight = 100;
+    
+    public static void main(String[] args) {
+        System.out.println(weight);
+    }
+}
+
+class Another {
+    
+}
