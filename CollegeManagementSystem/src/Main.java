20664c2ed2b4a8c0426764b368a81cb6a09afc68
Hasibul_Islam <43068575+Hasibull@users.noreply.github.com>
2023-07-27 16:34:45 +0600

    Deleting a project

--- CollegeManagementSystem/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        HomePage home = new HomePage();
-        
-        home.setVisible(true);
-        home.setLocationRelativeTo(null);
-    }
-}
